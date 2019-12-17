8d693f6220f379ab97156b977976a5771153a447
Rabah Ghodbane <r-ghodbane@live.com>
2019-12-17 13:42:07 +0100

    replace starter

---
+++ config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  siteTitle: 'Rabah Ghodbane', // <title>
+  manifestName: 'Rabah',
+  manifestShortName: 'Rabah', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: null,
+  firstName: 'Rabah',
+  lastName: 'Ghodbane',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/arabsight',
+    },
+    {
+      icon: 'fa-linkedin-in',
+      name: 'Linkedin',
+      url: 'https://linkedin.com/in/rabah-ghodbane-1a361a141/',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/arabsight',
+    },
+  ],
+  email: 'r-ghodbane@live.com',
+  phone: '+213 (0)559 17 77 94',
+  address: "N'GAOUS BATNA, ALGERIA",
+};
